c644cec87d1c4d8467e57837bdec35c57dca0de5
yasir satti <yassir.satti@hotmail.com>
2022-08-20 23:22:57 +0100

    Refactor by extracting building basket into private method

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Practicing TDD with Java
+# Practicing TDD with Java: Shopping basket totals
 
-TDD makes you make:
+TDD makes you:
 
 1. Write classes/methods/getters/setters in the test, but obviously fails becuase no code is written yet. So to make the test pass it forces you to go and write the code for these elements. So it forces you to write minimul code.
 2. Make design decisions as you make your test pass
@@ -13,3 +13,25 @@ TDD golden rule
 Writing a test and it fails !
 
 Start adding methods and other classes as we attempt to make the tests pass
+
+## Test 1 & 2
+
+Wrote test 1 for an empty shopping basket and it fails !
+
+Wrote code and made it pass
+
+Write test 2 for a basket with 1 item and it fails 
+
+Wrote code and made it pass
+
+## Test 3
+
+Wrote test 3 for basket with two items and failed
+
+Wrote code and made it pass
+
+## Refactor
+
+Duplicated code in every test when creating a new basket
+
+exctracted into a private method and passed basket items as paramemter
--- src/test/java/ShoppingBasketTest.java
+++ src/test/java/ShoppingBasketTest.java
@@ -9,24 +9,27 @@ public class ShoppingBasketTest {
 
     @Test
     public void totalOfEmptyBasket(){
-        ShoppingBasket basket = new ShoppingBasket(new ArrayList<>());
+        ShoppingBasket basket = buildBasketWithItems();
         assertEquals(0.0, basket.getTotal(), 0.0);
     }
 
     @Test
     public void totalOfSingleItem(){
-        ShoppingBasket basket = new ShoppingBasket(Arrays.asList(new Item(100.0, 1)));
+        ShoppingBasket basket = buildBasketWithItems(new Item(100.0, 1));
         assertEquals(100.0, basket.getTotal(), 0.0);
     }
 
     @Test
     public void totalOfTwoItems(){
-        ShoppingBasket basket = new ShoppingBasket(
-                Arrays.asList(
-                        new Item(100.0, 1),
-                        new Item(200.0, 1))
+        ShoppingBasket basket = buildBasketWithItems(
+                    new Item(100.0, 1),
+                    new Item(200.0, 1)
         );
 
         assertEquals(300.0, basket.getTotal(), 0.0);
+    }
+
+    private ShoppingBasket buildBasketWithItems(Item... items) {
+        return new ShoppingBasket(Arrays.asList(items));
     }
 }
