0b915fff6136ed208fa1dfdc018d800ea6b5477f
yasir satti <yassir.satti@hotmail.com>
2022-08-20 02:06:53 +0100

    passed test 1 and 2

---
+++ src/main/java/Item.java
@@ -0,0 +1,12 @@
+public class Item{
+
+    private final double unitPrice;
+
+    public Item(double unitPrice, int quantity) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+}
---
+++ src/test/java/ShoppingBasketTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShoppingBasketTest {
+
+    @Test
+    public void totalOfEmptyBasket(){
+        ShoppingBasket basket = new ShoppingBasket(new ArrayList<>());
+        assertEquals(0.0, basket.getTotal(), 0.0);
+    }
+
+    @Test
+    public void totalOfSingleItem(){
+        ShoppingBasket basket = new ShoppingBasket(Arrays.asList(new Item(100.0, 1)));
+        assertEquals(100.0, basket.getTotal(), 0.0);
+    }
+}
