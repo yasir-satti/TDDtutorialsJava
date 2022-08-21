5721124398c78e5aca00a8ecff63163ce4783433
yasir satti <yassir.satti@hotmail.com>
2022-08-21 01:59:43 +0100

    tdd habits 4 efficient assetion

--- src/main/java/DonateMovie/Movie.java
+++ src/main/java/DonateMovie/Movie.java
@@ -1,4 +1,13 @@
 package DonateMovie;
 
 public class Movie {
+    private int copies;
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void addCopy() {
+        copies++;
+    }
 }
